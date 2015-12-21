23e865f54df95c08d036508e3e55201d9580b44f
rokibul hasan <rakib063049@gmail.com>
2015-12-21 18:13:19 +0600

    New method #calculate_score  should calculate score on current roll and count non-scoring dice according given business logic

--- roll.rb
+++ roll.rb
@@ -6,4 +6,27 @@ class Roll
     dice.times { @values << rand(1..6) }
     @values
   end
+
+  def calculate_score
+    score = 0
+    numbers = Hash.new 0
+    non_scoring, non_scoring_numbers = 0, [2, 3, 4, 6]
+
+    @values.each do |n|
+      numbers[n] += 1
+      if numbers[n] == 3
+        score += (n == 1) ? 1000 : 100 * n
+        numbers[n] = 0
+      end
+    end
+
+    score += numbers[1] * 100
+    score += numbers[5] * 50
+
+    non_scoring_numbers.each do |n|
+      non_scoring += numbers[n]
+    end
+
+    [score, non_scoring]
+  end
 end
--- spec/roll_spec.rb
+++ spec/roll_spec.rb
@@ -20,4 +20,26 @@ describe Roll do
       expect(@roll.values.min).to be_between(1, 6)
     end
   end
+
+  describe "#calculate_score" do
+    it "should return an array" do
+      expect(@roll.calculate_score).to be_a(Array)
+    end
+
+    it "should return [score, non_scoring]" do
+      expect(@roll.calculate_score.size).to eql(2)
+    end
+
+    it "should calculate score on current roll and count non-scoring dice" do
+      @roll.instance_eval('@values = [5, 1, 3, 4, 1]')
+      expect(@roll.calculate_score).to eql([250, 2])
+
+      @roll.instance_eval('@values = [1, 1, 1, 3, 1]')
+      expect(@roll.calculate_score).to eql([1100, 1])
+
+      @roll.instance_eval('@values = [2, 4, 4, 5, 4]')
+      expect(@roll.calculate_score).to eql([450, 1])
+    end
+  end
+
 end
