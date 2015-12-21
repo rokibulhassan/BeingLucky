a84618462984cce0717271fd69de68caf540f4ef
rakib hasan <rakib063049@gmail.com>
2015-12-21 22:50:27 +0600

    Play game, count accruing point when point exceed 300, enter the final round while points exced 3000, implement #score method

--- being_lucky.rb
+++ being_lucky.rb
@@ -1,14 +1,69 @@
 class BeingLucky
-  attr_reader :dice
+  attr_reader :dice, :players, :scores, :in_the_game, :final_round
 
   def initialize(dice=nil)
     @dice = dice||5
+    @players = ['Bob', 'Woofer']
+    @scores = Hash.new 0
+    @in_the_game = false
+    @final_round = false
   end
 
   def score
-
+    self.play_game
+    winning_score = scores.sort { |a, b| b[1]<=>a[1] }.first
+    puts "\nWinner :: #{winning_score[0]} scored #{winning_score[1]} points!"
   end
 
+  def play_game
+    display_players
+
+    while players.size > 0
+      player = @players.shift
+      players.push(player) unless @final_round
+      puts "\n\n"
+
+      points = take_a_turn(player)
+      display_result(player, points)
+      accruing_points(player, points)
+      play_final_round(player)
+    end
+  end
+
+  def display_players
+    puts "\n Players #{self.players.join(', ')} are playing Being Lucky"
+  end
+
+  def take_a_turn(player)
+    Turn.new(self.dice, player).start
+  end
+
+  def display_result(player, points)
+    result = "#{player} score: #{points} points"
+    if scores[player] == 0
+      if 0 < points && points < 300
+        result += " and not counting"
+      elsif points >= 300
+        result += " and * Starts * counting"
+      end
+    end
+    puts result
+  end
+
+  def accruing_points(player, points)
+    if scores[player] > 0 || points >= 300
+      scores[player] += points
+      @in_the_game = true
+    end
+    puts "#{player} has #{scores[player]}"
+  end
+
+  def play_final_round(player)
+    if scores[player] >= 3000
+      puts "\n... Playing * Final * round ..." unless @final_round
+      @final_round = true
+    end
+  end
 end
 
 
--- spec/being_lucky_spec.rb
+++ spec/being_lucky_spec.rb
@@ -16,4 +16,35 @@ describe BeingLucky do
     end
   end
 
+  describe "#accruing_points" do
+    before :each do
+      @being_lucky.accruing_points('Bob', 302)
+      @score = @being_lucky.scores.sort { |a, b| b[1]<=>a[1] }.first
+    end
+
+    it "should start point counting, if points >= 300" do
+      expect(@score[1]).to be >= 300
+      expect(@being_lucky.in_the_game).to be true
+    end
+  end
+
+  describe "#play_final_round" do
+    before :each do
+      @being_lucky.instance_eval("@scores = {'Bob' => 3002, 'Woofer' => 2700}")
+      @being_lucky.play_final_round('Bob')
+      @score = @being_lucky.scores.sort { |a, b| b[1]<=>a[1] }.first
+    end
+
+    it "should enter in final round if scores >= 3000" do
+      expect(@score[1]).to be >= 3000
+      expect(@being_lucky.final_round).to be true
+    end
+  end
+
+  describe "#score" do
+    it "should scored for the game" do
+      @being_lucky.score
+    end
+  end
+
 end
